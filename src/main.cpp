254aff0fc36f61f4246acbc8ba8fde5110f8f30b
LCottin <cottin_leonard@icloud.com>
2021-06-04 23:25:44 +0200

    Added a dice under test

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,20 @@
-/*******************
- *    MAIN FILE    *
- ******************/
+/************************
+ *      MAIN FILE       *
+ ************************/
 
 #include <iostream>
 #include <string>
+
+#include "Dice.hpp"
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
+
+    Dice d;
+    d.roll();
+
     cout << "Runned main" << endl;
     return 0;
 }
