0268d2b8e0be53706278a92dde54c9cd24ca269f
LCottin <cottin_leonard@icloud.com>
2021-10-25 22:18:59 +0200

    Small Optimization in constructor

--- src/Piece.cpp
+++ src/Piece.cpp
@@ -9,65 +9,55 @@ Piece::Piece()
 
 Piece::Piece(const TYPES type)
 {
-    //sets initial position at 0, 0
-    _Position   = Vector2f(0, 0);
-    _Scale      = Vector2f(0.2, 0.2);
+    //sets initial position 
+    _Position   = Vector2f(750, 750);
+    _Scale      = Vector2f(0.1, 0.1);
+    _Type       = type;
 
     //loads correct texture
     switch (type)
     {
         case BARROW:
-            _Type = BARROW;
             _Texture.loadFromFile("./Images/Pieces/barrow.png");
             break;
         
         case BASEBALL:
-            _Type = BASEBALL;
             _Texture.loadFromFile("./Images/Pieces/baseball.png");
             break;
 
         case CAR:
-            _Type = CAR;
             _Texture.loadFromFile("./Images/Pieces/car.png");
             break;
 
         case DOG:
-            _Type = DOG;
             _Texture.loadFromFile("./Images/Pieces/dog.png");
             break;
 
         case DRAGON:
-            _Type = DRAGON;
             _Texture.loadFromFile("./Images/Pieces/dragon.png");
             break;
 
         case HAT:
-            _Type = HAT;
             _Texture.loadFromFile("./Images/Pieces/hat.png");
             break;
 
         case PRETZEL:
-            _Type = PRETZEL;
             _Texture.loadFromFile("./Images/Pieces/pretzel.png");
             break;
 
         case SHIP:
-            _Type = SHIP;
             _Texture.loadFromFile("./Images/Pieces/ship.png");
             break;
 
         case SOCCER:
-            _Type = SOCCER;
             _Texture.loadFromFile("./Images/Pieces/soccer.png");
             break;
 
         case SURFER:
-            _Type = SURFER;
             _Texture.loadFromFile("./Images/Pieces/surfer.png");
             break;
 
         default:
-            _Type = THIMBLE;
             _Texture.loadFromFile("./Images/Pieces/thimble.png");
             break;
     }
