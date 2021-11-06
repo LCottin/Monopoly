7a4e2f49df328f48475660b29d12b096c6581750
LCottin <cottin_leonard@icloud.com>
2021-11-06 22:11:48 +0100

    Minor changes

--- src/Game.cpp
+++ src/Game.cpp
@@ -24,32 +24,28 @@ Game::Game()
  */
 void Game::getNbPlayers()
 {    
-    // cout << "How many players will play ? " << endl;
-    // do
-    // {
-    //     cout << "Enter a number between 2 and 4 included. " << endl;
-    //     cout << "Number = ";
-    //     cin >> _NbPlayers;
-    // } while (_NbPlayers < 2 || _NbPlayers > 4);
+    cout << "How many players will play ? " << endl;
+    do
+    {
+        cout << "Enter a number between 2 and 4 included. " << endl;
+        cout << "Number = ";
+        cin >> _NbPlayers;
+    } while (_NbPlayers < 2 || _NbPlayers > 4);
     
-    // //creates all players
-    // for (size_t i = 0; i < _NbPlayers; i++)
-    // {
-    //     //FIXME: avoid player to have the same piece by removing it from the available pieces
-    //     string name = "none";
-    //     do
-    //     {
-    //         cout << "Player " << i + 1 << ", what's your name ?" << endl;
-    //         cout << "Name : ";
-    //         getline(cin, name);
-    //     } while(name == "none" || name == "");
+    //creates all players
+    for (size_t i = 0; i < _NbPlayers; i++)
+    {
+        string name = "none";
+        do
+        {
+            cout << "Player " << i + 1 << ", what's your name ?" << endl;
+            cout << "Name : ";
+            getline(cin, name);
+        } while(name == "none" || name == "");
 
-    //     Player* pl = new Player(name);
-    //     _Players.push_back(pl);
-    // }
-    Player* pl = new Player("test", BASEBALL);
-    _Players.push_back(pl);
-    _NbPlayers = (int)_Players.size();
+        Player* pl = new Player(name);
+        _Players.push_back(pl);
+    }
     cout << "Let the game begin ! " << endl;
 }
 
