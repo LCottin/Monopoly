eb9ee59209e4fb3301e4e95db0ecf3b28112f003
LCottin <cottin_leonard@icloud.com>
2021-11-06 18:50:30 +0100

    Created PLACES.hpp

---
+++ src/PLACES.hpp
@@ -0,0 +1,60 @@
+/********************
+ *      PLACES      *
+ ********************/
+
+#ifndef __PLACES__
+#define __PLACES__
+
+enum PLACES
+{
+    //last line
+    GO                      = 0,
+    MEDITERRANEAN_AVENUE    = 1,
+    COMMUNITY_CHEST_1       = 2,
+    BALTIC_AVENUE           = 3,
+    INCOME_TAX              = 4,
+    READING_RAILROAD        = 5,
+    ORIENTAL_AVENUE         = 6,
+    CHANCE_1                = 7,
+    VERMONT_AVENUE          = 8,
+    CONNECTICUT_AVENUE      = 9,
+
+    //first row
+    JAIL                    = 10,
+    ST_CHARLES_PLACE        = 11,
+    ELECTRIC_COMPANY        = 12,
+    STATES_AVENUE           = 13,
+    VIRGINIA_AVENUE         = 14,
+    PENNSYLVANIA_RAILROAD   = 15,
+    ST_JAMES_PLACE          = 16,
+    COMMUNITY_CHEST_2       = 17,
+    TENNESSEE_AVENUE        = 18,
+    NEW_YORK_AVENUE         = 19,
+
+    //first line
+    FREE_PARKING            = 20,
+    KENTUCKY_AVENUE         = 21,
+    CHANCE_2                = 22,
+    INDIANA_AVENUE          = 23,
+    ILLINOIS_AVENUE         = 24,
+    B_AND_O_RAILROAD        = 25,
+    ATLANTIC_AVENUE         = 26,
+    VENTNOR_AVENUE          = 27,
+    WATER_WORKS             = 28,
+    MARVIN_GARDENS          = 29,
+
+
+    //last row
+    GO_TO_JAIL              = 30,
+    PACIFIC_AVENUE          = 31,
+    NORTH_CAROLINA_AVENUE   = 32,
+    COMMUNITY_CHEST_3       = 33,
+    PENNSYLVANIA_AVENUE     = 34,
+    SHORT_LINE_RAILROAD     = 35,
+    CHANCE_3                = 36,
+    PARK_PLACE              = 37,
+    LUXURY_TAX              = 38,
+    BOARDWALK               = 39
+};
+
+#endif
