686c8d245e402a56d80af6561869852c28f7e254
LCottin <cottin_leonard@icloud.com>
2021-12-14 18:24:05 +0100

    Cleaned main

--- src/main.cpp
+++ src/main.cpp
@@ -19,57 +19,13 @@ using namespace sf;
 
 int main(int argc, char const *argv[])
 {
-    Game g;
-    //g.getNbPlayers();
-    g.playGame();
-    //g.rollDices();
-/*
-    //Creation of a window to try drawing the dice
-    RenderWindow window(VideoMode(800, 800), "try");
-    window.setFramerateLimit(50);
-
-    //creation
-    Dice d;
-    Player* pl = new Player("Test", CAR);
-    Communities com;
-    Chances ch;
+    Game monopoly;
+    bool run = monopoly.playGame();
     
-    Texture font;
-    font.loadFromFile("./Images/Boards/monopoly.png");
-    
-    Sprite bg;
-    bg.setTexture(font);
+    if (run)
+        cout << "Game finished." << endl;
+    else
+        cout << "Game aborted." << endl;
 
-
-    while (window.isOpen())
-    {
-        Event event;
-
-        while (window.pollEvent(event))
-        {
-            if (event.type == Event::Closed)
-            {
-                window.close();
-            }
-        } 
-        
-        window.clear();
-        window.draw(bg);
-        window.draw(*com.getMainSprite());
-        window.draw(*ch.getMainSprite());
-        ch.drawCard();
-        
-        d.roll();
-        window.draw(*d.getSprite());
-        
-        pl->getPiece()->move(Vector2f(100, 100));
-        window.draw(*pl->getPiece()->getSprite());
-        
-        window.display();
-    }
-
-    delete pl;
-*/
-    cout << "Runned main" << endl;
     return 0;
 }
